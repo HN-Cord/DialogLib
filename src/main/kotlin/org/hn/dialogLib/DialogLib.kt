ea66804098488526d5640d0463b7cb8d1d41d77c
嘿夜 <97263430+HeyNight0712@users.noreply.github.com>
2025-08-20 21:22:50 +0800

    feat: 初始化專案，新增基本結構與配置

---
+++ src/main/kotlin/org/hn/dialogLib/DialogLib.kt
@@ -0,0 +1,14 @@
+package org.hn.dialogLib
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DialogLib : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
